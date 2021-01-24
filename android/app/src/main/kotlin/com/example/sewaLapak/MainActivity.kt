d8d5ef02a2dea80028bfcabbaf17cee81219aeb2
Fakhri Ahnaf <fakhriahnaf@Fakhris-MacBook-Air.local>
2021-01-24 23:42:44 +0700

    framework commit

---
+++ android/app/src/main/kotlin/com/example/sewaLapak/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sewaLapak
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color maroonColor = Color(0xffBE4343);
+Color orangeColor = Color(0xffFF9376);
+Color blackColor = Color(0xff000000);
+Color whiteColor = Color(0xFFFFFFFF);
+Color greyColor = Color(0xFF82868E);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: blackColor,
+);
+
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w300,
+  color: greyColor,
+);
+
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: whiteColor,
+);
+
+TextStyle maroonTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: maroonColor,
+);
